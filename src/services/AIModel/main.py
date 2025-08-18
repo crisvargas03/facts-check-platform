8ac3dc71dddc03e9aa8958793f6f5d7a98713c69
Leonal Perez <karplincontact@gmail.com>
2025-08-17 22:56:06 -0400

    feat: adjust .env.example and response in main

--- src/services/AIModel/main.py
+++ src/services/AIModel/main.py
@@ -17,13 +17,16 @@ def chat(request: ArticleRequest):
             "error": "",
             "success": True,
             "result": {
-                "percentaje_trust": data["percentaje-trust"],
-                "reliable_source": data["reliable-source"],
-                "scientific_evidence": data["scientific-evidence"],
-                "citations_and_references": data["citations-and-references"],
-                "target_language": data["target-language"],
-                "context_and_limitations": data["context-and-limitations"],
-                "feedback": data["feedback"]
+                "motive": data["motive"],
+                "percentage_trust": data["percentage-trust"],
+                "evaluation_factors": [
+                    {
+                        "title": factor["title"],
+                        "description": factor["descripcion"],
+                        "evaluation_result": factor["evaluation-result"]
+                    }
+                    for factor in data["evaluation-factors"]
+                ]
             }
         }
         
